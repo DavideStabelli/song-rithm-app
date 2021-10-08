7ed0ce83420dccf807bffd2807c0cc1bee9baaae
Davide Stabelli <dst@huko.hk>
2021-10-08 14:09:57 +0200

    * added circle png

--- core/src/it/davidestabelli/songrithmapp/Main.java
+++ core/src/it/davidestabelli/songrithmapp/Main.java
@@ -12,12 +12,12 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Circle_(transparent).png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
