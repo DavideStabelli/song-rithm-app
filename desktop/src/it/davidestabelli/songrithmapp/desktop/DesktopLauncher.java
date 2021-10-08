2b002830e6e628832d102f33dbed4feb0e326d4c
Davide Stabelli <dst@huko.hk>
2021-10-08 13:51:31 +0200

    * first commit with new project

---
+++ desktop/src/it/davidestabelli/songrithmapp/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package it.davidestabelli.songrithmapp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.davidestabelli.songrithmapp.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.resizable = false;
+		new LwjglApplication(new Main(), config);
+	}
+}
